b72cfc99aefffdd3e2a5b959a8cce548fbfc0fc0
krupin.dv <krupin.dv19@gmail.com>
2015-04-02 20:44:54 +0300

    phones task

---
+++ utils/phones.sql
@@ -0,0 +1,40 @@
+ALTER TABLE phones ADD PRIMARY KEY (phone);
+ALTER TABLE items ADD PRIMARY KEY (id);
+
+
+INSERT INTO phones (phone, users) VALUES ('123', '{1,2,3}');
+
+INSERT INTO phones (phone, users) VALUES ('456', '{4,5,6}');
+INSERT INTO phones (phone, users) VALUES ('789', '{7,8,9}');
+
+
+
+INSERT INTO items (user_id, status) VALUES (1, 1);
+
+
+
+SELECT
+    phone, unnest(users) as user_id, status
+FROM phones
+LEFT JOIN items ON (user_id = items.user_id AND items.status=1)
+
+
+
+SELECT
+    phone, user_id
+FROM (SELECT phone, unnest(users) as user_id FROM phones)
+LEFT JOIN items ON (user_id = items.user_id AND items.status=1)
+
+
+
+SELECT
+    p.phone, COUNT(*) as items_sold
+FROM (SELECT phone, unnest(users) as user_id FROM phones) p
+JOIN items ON (p.user_id = items.user_id AND items.status=1)
+GROUP BY phone
+
+
+
+
+
+
