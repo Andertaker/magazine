3c7711ec672b8afb0cec96cc92aa84e4c996bc3d
krupin.dv <krupin.dv19@gmail.com>
2015-04-02 18:22:10 +0300

    magazin admin

--- magazine/admin.py
+++ magazine/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Category, Product
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_fields = ['name', ]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_fields = ['category', 'name', 'price']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
