42bf6d16dd67479d35fd4dc5767ec87173c63a99
krupin.dv <krupin.dv19@gmail.com>
2015-04-02 18:21:56 +0300

    magazine models added (Category, Product)

--- magazine/models.py
+++ magazine/models.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(u'Категория товара', max_length=64)
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, verbose_name=u'Категория')
+    name = models.CharField(u'Наименование товара', max_length=128)
+    price = models.DecimalField(u'Цена единицы, руб.', max_digits=10, decimal_places=2)
